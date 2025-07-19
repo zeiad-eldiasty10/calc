874d0cf64389301417bfcaebc98cde74219a3a70
zeiad <zeiadeldiasty72@gmail.com>
2025-07-19 18:14:57 +0300

    calc

---
+++ app.js
@@ -0,0 +1,73 @@
+const screen = document.getElementById('screen');
+const buttons = document.querySelectorAll('.btn');
+
+let currentInput = '';
+let previousInput = '';
+let operator = '';
+
+function updateScreen(value) {
+    screen.value = value;
+}
+
+function calculate() {
+    if (previousInput !== '' && currentInput !== '') {
+        switch (operator) {
+            case '+':
+                currentInput = parseFloat(previousInput) + parseFloat(currentInput);
+                break;
+            case '-':
+                currentInput = parseFloat(previousInput) - parseFloat(currentInput);
+                break;
+            case '*':
+                currentInput = parseFloat(previousInput) * parseFloat(currentInput);
+                break;
+            case '/':
+                if (parseFloat(currentInput) === 0) {
+                    currentInput = 'Error'; 
+                } else {
+                    currentInput = parseFloat(previousInput) / parseFloat(currentInput);
+                }
+                break;
+            case '%':
+                currentInput = (parseFloat(previousInput) % parseFloat(currentInput));
+                break;
+            default:
+                break;
+        }
+        updateScreen(currentInput);
+        previousInput = '';
+        operator = '';
+    }
+}
+
+function handleButtonClick(event) {
+    const value = event.target.textContent;
+
+    if (value === 'C') {
+        currentInput = '';
+        previousInput = '';
+        operator = '';
+        updateScreen('0');
+    } else if (value === 'DEL') {
+        currentInput = currentInput.slice(0, -1);
+        updateScreen(currentInput || '0');
+    } else if (value === '=') {
+        calculate();
+    } else if (value === '.' && !currentInput.includes('.')) {
+        currentInput += value;
+        updateScreen(currentInput);
+    } else if (['+', '-', '*', '/', '%'].includes(value)) {
+        if (currentInput !== '') {
+            previousInput = currentInput;
+            currentInput = '';
+            operator = value;
+        }
+    } else {
+        currentInput += value;
+        updateScreen(currentInput);
+    }
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', handleButtonClick);
+});
